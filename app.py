7b51ce952a27360a9db5384568cc87fc673f129b
netstionke <netstionke@gmail.com>
2024-02-28 21:12:50 +0100

    fuck apache

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, redirect, render_template, session, flash
-import mysql.connector
+from flask import Flask, request, redirect, render_template, session,flash, url_for
+from utils.sql import dbJdr
+from fiche import *
+import logging
 import os
+import time
+import hashlib
 
 secret_key = os.urandom(20)
 
-app = Flask('__name__')
+app = Flask('__name__', template_folder="/var/www/html/jdr/templates")
 app.secret_key = secret_key
+database = dbJdr()
 
 mysql_config = {
     'host': 'localhost',
@@ -14,43 +19,73 @@ mysql_config = {
     'database': 'jdr'
 }
 def get_mysql_connection():
-    return mysql.connector.connect(**mysql_config)
+    return database.get_connection()
 
 
 @app.route('/')
 def main():
+    #app.logger.debug(url_for('static', filename='style.css'))
     if 'username' in session:
-            return render_template('index.html', username=session['username'])
+        return render_template('index.html', username=session['username'])
     else:
-          return render_template('index.html')
-    
+        return redirect('login')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'username' in session:
+        return redirect('/jdr')
     if request.method == 'POST':
         username = request.form['username'] #POST Du champ Username
-        password = request.form['password'] #POST du champ Password
-    
-        conn = get_mysql_connection() #Setup Connexion à MySQL
-        query = conn.cursor(dictionary=True) #Pointer qui permets de requetter la BDD
+        password = request.form['password'].encode('UTF-8') #POST du champ Password
 
-        query.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username,password)) #Requete qui va recupere User/PWD
-        user = query.fetchone()
-
-        query.close() #Fermetuer pointer
-        conn.close() #Fermeture Connexion
+        hash_object = hashlib.sha256(password)
+        hashed_password = hash_object.hexdigest()
+        user = database.execute('SELECT * FROM users WHERE username ="' + str(username) + '" AND password = "' + str(hashed_password) + '";') #Requete qui va recupere User/PWD
 
         if user:
-             session['username'] = user['username']
-             flash('T''es co batard', 'success')
-             return redirect('/')
+            session['username'] = user[0]['username']
+            flash('T''es co batard', 'success')
+            return redirect('/jdr')
         else:
-             flash('Mot de passe OU User invalide', 'error')
-             return redirect('/login')
+            flash('Mot de passe OU User invalide', 'error')
+            time.sleep(3)
+            return render_template('login.html')
     else:
         return render_template('login.html')
-        
-          
-    
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        new_username = request.form['new_username']
+        new_password = request.form['new_password'].encode('utf-8')
+        #Test si user déjà existant
+        existing_user = database.execute('SELECT * FROM users WHERE username ="' + str(new_username) + '" ;')
+
+
+        if existing_user:
+            flash('User déjà Existant', 'error')
+            return render_template("signup.html")
+        else:
+            hash_object = hashlib.sha256(new_password)
+            hashed_password = hash_object.hexdigest()
+
+            database.execute('INSERT INTO users (username, password) VALUES ("' + str(new_username) + '", "' + str(hashed_password) +  '");')
+            flash('Compte crée')
+            return render_template('login.html')
+    return render_template("signup.html")
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.clear()
+    return redirect('/jdr')
+
+@app.route('/fiches')
+def fiches():
+    #return fiching(app)
+    pass
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ utils/sql.py
@@ -0,0 +1,44 @@
+import mysql.connector
+
+class dbJdr:
+    connection = None
+
+    def connect(self):
+        if self.is_connected() == None:
+            return self.get_connection()
+        host = 'localhost'
+        user = 'root'
+        passwd = '18122001Bd-'
+
+        for i in range(3):
+            try:
+                self.connection = mysql.connector.connect(host=host, user=user, password=passwd, database="jdr")
+                return (self.connection)
+            except:
+                pass
+        print("Connection to database failded !")
+        return None
+
+    def is_connected(self):
+        if self.get_connection() == None:
+            return False
+        return True
+
+    def get_connection(self):
+        return self.connection
+
+    def execute(self, command, dictionary=True, raw=False):
+        if not self.is_connected():
+            if self.connect() == None:
+                print("execution db error")
+                return None
+        cursor = self.connection.cursor(dictionary=dictionary, raw=raw)
+        #cursor.execute("USE odium;")
+        cursor.execute(command)
+        liste = []
+        for i in cursor:
+            liste.append(i)
+            print(i)
+        cursor.close()
+        self.connection.commit()
+        return liste
